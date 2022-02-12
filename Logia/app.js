b9431608fa4556f14ab20fe2bf3e081f98098b6a
Burguiere Corentin <corentin.burguiere@universite-paris-saclay.fr>
2022-02-12 22:07:24 +0100

    Récupère les infos à interval régulier

--- Logia/app.js
+++ Logia/app.js
@@ -22,51 +22,55 @@ const options = {
     "path": "/weather?q=London%2Cuk&lang=fr",
     "headers": {
         "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-        "x-rapidapi-key": "90ff7ee5c2msh4e0de3b1449b49fp1ee4ccjsnabaada3c348b",
+        "x-rapidapi-key": "--",
         "useQueryString": true
     }
 };
 
-//Get information from the API
-const req = https.request(options, function (res) {
-    const chunks = [];
+var interval = setInterval(function () {
+    //Get information from the API
+    const req = https.request(options, function (res) {
+        const chunks = [];
 
-    res.on("data", function (chunk) {
-        chunks.push(chunk);
+        res.on("data", function (chunk) {
+            chunks.push(chunk);
+        });
+
+        res.on("end", function () {
+            const body = Buffer.concat(chunks);
+
+            //Uncommented to see all data on the console
+            //console.log(body.toString());
+
+            const parsedData = JSON.parse(body);
+            console.log(parsedData);
+            //Collect data on the parsedData
+            city = parsedData.name;
+            meteo = parsedData.weather[0].main;
+
+        
+            console.log(city);
+            console.log(meteo);
+        
+            console.log(i);
+            i++;
+        });
     });
+    req.end();
 
-    res.on("end", function () {
-        const body = Buffer.concat(chunks);
-
-        //Uncommented to see all data on the console
-        //console.log(body.toString());
-
-        const parsedData = JSON.parse(body);
-
-        //Collect data on the parsedData
-        city = parsedData.name;
-        meteo = parsedData.weather[0].main;
-
-        /*
-        console.log(city);
-        console.log(meteo);
-        */
-        i++;
-        console.log(i);
-    });
-});
 
 io.on('connection', socket => {
 
-    var interval = setInterval(function () {
+    
         data = meteo + " " + i;
 
         console.log("SENT: " + data);
 
         socket.emit('afficherMeteo', data);
-    }, 10);
+    
 
 })
+}, 1000);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/html/index.html');
@@ -76,5 +80,5 @@ server.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
 })
 
-req.end();
+
 module.exports = app;
