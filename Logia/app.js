b8ce0d3d5f0b70d370c01b88ba0a3e18bce7b86e
Burguiere Corentin <corentin.burguiere@universite-paris-saclay.fr>
2022-02-13 17:04:49 +0100

    Version Finale

--- Logia/app.js
+++ Logia/app.js
@@ -1,4 +1,6 @@
-﻿const express = require('express');
+﻿//Défi - Soixante Circuits - Corentin Burguiere le 13 février 2022
+
+const express = require('express');
 const app = express();
 const http = require("http");
 const https = require("https");
@@ -8,86 +10,104 @@ const port = 3000
 const server = http.createServer(app);
 const io = new Server(server);
 
-var data = "Real-Time Update 1";
-
-var city = '';
-var meteo = '';
-var desc = '';
-var temp = '';
-var waitTime = 10000;
+//Waiting time before change city (in milliseconds)
+var waitTime = 30000;
 var seconds = waitTime / 1000;
 
-app.use('/', express.static('public'));
+//Connection and deconnection
+    app.use('/', express.static('public'));
 
-const options = {
-    "method": "GET",
-    "hostname": "community-open-weather-map.p.rapidapi.com",
-    "port": null,
-    //"path": "/weather?q=London%2Cuk&lang=fr",
-    "path": "/weather?",
-    "headers": {
-        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-        "x-rapidapi-key": "A COMPLETER",
-        "useQueryString": true
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/html/index.html');
+    });
+
+    server.listen(port, () => {
+        console.log(`Listening at http://localhost:${port}`)
+    })
+
+    io.on('connection', (socket) => {
+        console.log('a user connected');
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+            socket.disconnect();
+            socket.removeAllListeners();
+        });
+    });
+
+//API's options
+    const options = {
+        "method": "GET",
+        "hostname": "community-open-weather-map.p.rapidapi.com",
+        "port": null,
+        "path": "/weather?",
+        "headers": {
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+            "x-rapidapi-key": "A COMPLETER !",
+            "useQueryString": true
+        }
+    };
+
+//Choose a random city of the list
+    function villeAleatoire() {
+
+        //Init the link, then choose the city randomly
+        options.path = "/weather?";
+        var cityList = ["Paris", "London", "Tokyo", "New+York+City", "Moscow", "Nantes", "Florence", "Belgrade", "Madrid", "Berlin", "Cologne", "Amsterdam", "Los+Angeles", "Inuvik", "Barrow", "Mould+Bay", "Anadyr", "Seoul", "Sendai", "Singapore"];
+        var size = cityList.length - 1;
+        var random = randomIntFromInterval(0, size);
+
+        //Create URL with the city
+        var URLcity = cityList[random];
+        options.path = options.path + "q=" + URLcity + "&lang=fr";
+        console.log(options.path);
     }
-};
 
-villeAleatoire();
+    function randomIntFromInterval(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
 
-//Choose a random city on the list
-function villeAleatoire() {
+//Do once to not get a empty page at the start
 
-    //Init the link, then choose the city randomly
-    options.path = "/weather?";
-    var cityList = ["Paris", "London", "Tokyo", "New+York+City", "Moscow", "Nantes", "Florence", "Belgrade", "Madrid", "Berlin", "Cologne", "Amsterdam", "Los+Angeles", "Inuvik", "Barrow", "Mould+Bay", "Anadyr", "Seoul", "Sendai", "Singapore"];
-    var size = cityList.length - 1;
-    var random = randomIntFromInterval(0, size);
-
-    //Create URL with the city
-    var URLcity = cityList[random];
-    options.path = options.path + "q=" + URLcity + "&lang=fr";
-    console.log(options.path);
-}
-
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
+//Start with a random city
+villeAleatoire(); 
 
 io.on('connection', (socket) => {
+    //Send the seconds to the client
+    seconds = waitTime / 1000;
     socket.emit('chrono', seconds);
-    //Do once to not get a empty page at the start
+    
     var req = https.request(options, function (res) {
         var chunks = [];
 
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
+       res.on("data", function (chunk) {
+                chunks.push(chunk);
+            });
+
+            res.on("end", function () {
+                var body = Buffer.concat(chunks);
+
+                var parsedData = JSON.parse(body);
+                //Uncommented to see all data on the console
+                //console.log(parsedData);
+
+                //Collect data on the parsedData
+                city = parsedData.name;
+                meteo = parsedData.weather[0].main;
+                desc = parsedData.weather[0].description;
+                temp = parsedData.main.temp;
+
+                console.log("Ville : " + city);
+                console.log("Météo actuelle : " + meteo);
+                console.log("Description : " + desc);
+                console.log("Température : " + temp);
+                console.log("-----------------------------");
+
+                //Send information to client
+                data = [city, meteo, desc, temp];
+                socket.emit('afficherMeteo', data);
+            });
         });
-
-        res.on("end", function () {
-            var body = Buffer.concat(chunks);
-
-            var parsedData = JSON.parse(body);
-            //Uncommented to see all data on the console
-            console.log(parsedData);
-
-            //Collect data on the parsedData
-            city = parsedData.name;
-            meteo = parsedData.weather[0].main;
-            desc = parsedData.weather[0].description;
-            temp = parsedData.main.temp;
-
-            console.log("Ville : " + city);
-            console.log("Météo actuelle : " + meteo);
-            console.log("Description : " + desc);
-            console.log("Température : " + temp);
-            console.log("-----------------------------");
-
-            //Send information to client
-            data = [city, meteo, desc, temp];
-            socket.emit('afficherMeteo', data);
-        });
-    });
-    req.end();
+        req.end();
 });
 
 //Search a new city every 30 secondes
@@ -128,8 +148,6 @@ io.on('connection', socket => {
                 seconds = waitTime / 1000;
                 socket.emit('afficherMeteo', data);
             });
-
-
         });
 
         io.on('close', function close() {
@@ -141,20 +159,31 @@ io.on('connection', socket => {
     }, waitTime);
 });
 
-io.on('connection', socket => {
+//Chronometre
+io.once('connection', socket => {
     var interval = setInterval(function () {
+
+        //Decrement time
         seconds--;
-        console.log("Une seconde est passée..." + seconds);
-        socket.emit('chrono', seconds);
+
+        if (seconds < 0) {
+            seconds = waitTime / 1000;
+
+            //Send the seconds to the client
+            socket.emit('afficherMeteo', data);
+        }
+        else {
+            console.log("Une seconde est passée..." + seconds);
+
+            //Send the seconds to the client
+            socket.emit('chrono', seconds);
+        }
+
+        io.on('close', function close() {
+            clearInterval(interval);
+        });
+
     }, 1000);
 });
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/html/index.html');
-});
-
-server.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
-})
 
 module.exports = app;
